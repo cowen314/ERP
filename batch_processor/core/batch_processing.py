0f91483dc57691041965845c3cfc7f7998bf8912
Christian Owen <cowen314@gmail.com>
2021-07-21 17:38:04 -0500

    fix minor issues in batch processor, update tool output reporting

--- batch_processor/core/batch_processing.py
+++ batch_processor/core/batch_processing.py
@@ -1,13 +1,14 @@
 import subprocess
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 # TODO handle labels
 
-ERP_EXE_NAME = "ERP"
+# ERP_EXE_NAME = "ERP"
+ERP_EXE_NAME = "echo"  # for use when testing. This'll just print the parameters to stdout. 
 
 
-def process_batch(subjects_directory: Path, output_directory: Path, segment_ids: List[int]) -> List[str], List[str]:
+def process_batch(subjects_directory: Path, output_directory: Path, segment_ids: List[int]) -> Tuple[List[str], List[str]]:
     """process a batch of patients all at once
 
     Args:
@@ -32,7 +33,7 @@ def process_batch(subjects_directory: Path, output_directory: Path, segment_ids:
     return results, errors
 
 
-def process_single(input_dir: Path, output_dir: Path, segment_ids: List[int], output_images: bool=False) -> str, bool:
+def process_single(input_dir: Path, output_dir: Path, segment_ids: List[int], output_images: bool=False) -> Tuple[str, bool]:
     """process a single patient
 
     Args:
@@ -43,16 +44,24 @@ def process_single(input_dir: Path, output_dir: Path, segment_ids: List[int], ou
         str, bool: status message, status bool (True = pass, False = fail)
     """
     # for readiability, split things out into a bunch of variables
-    # erp_cmd = []
-    executable_name = ERP_EXE_NAME
+    for i in range(len(segment_ids)):
+        segment_ids[i] = str(segment_ids[i])
     segments_arg = ",".join(segment_ids)
-    output_images_arg = 1 if output_images else 0
+    output_images_arg = '1' if output_images else '0'
     erp_cmd = [
-        executable_name,
+        ERP_EXE_NAME,
         segments_arg,
         output_images_arg
     ]
-    po = subprocess.run(erp_cmd, capture_output=True, cwd=input_dir)
+    
+    ## capture the output, deal with it later
+    # po = subprocess.run(erp_cmd, capture_output=True, cwd=input_dir)
+    # if po.returncode > 0:
+    #     return po.stdout.decode('ascii'), False
+    # return po.stdout.decode('ascii'), True
+
+    ## send the output directly to stdout
+    po = subprocess.run(erp_cmd, capture_output=False, cwd=input_dir)  # TODO send output to terminal AND capture?
     if po.returncode > 0:
-        return po.stdout.decode('ascii'), False
-    return po.stdout.decode('ascii'), True
+        return f"Error while processing '{str(input_dir.resolve())}'", False
+    return f"Processed '{str(input_dir.resolve())}' successfully", True
--- batch_processor/tests.py
+++ batch_processor/tests.py
@@ -1,9 +1,11 @@
-from .batch_processing import process_batch, process_single
 from pathlib import Path
+from core.batch_processing import process_batch, process_single
 
-process_single(
-    Path("../../erp-data/bert-20210604T031952Z-001/bert/mri"),
-    Path("../../erp-data/batch-process-outputs"),
+output, passed = process_single(
+    Path("/home/cowen/projects/erp-data/bert-20210604T031952Z-001/bert/mri"),
+    Path("/home/cowen/projects/erp-data/batch-process-outputs"),
     [53, 10]
 )
 # process_batch(Path()) # try this once we have a few more patients to run this on
+print(output)
+print(passed)
