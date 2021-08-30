bdca30211c17d5364d72e52bea74dabf2faea373
Christian Owen <cowen314@gmail.com>
2021-08-29 21:07:29 -0500

    update util with latest comparison files + start stop values

--- util/feature_error.py
+++ util/feature_error.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 from typing import List, Tuple
 
-truth = Path("/home/cowen/projects/erp-data/bert-standard-processed/rawavg_features.csv")
-test = Path("/home/cowen/projects/erp-data/bert-roi-processed/rawavg_features.csv")
-out_f = Path("/home/cowen/projects/erp-data/manual-lhip-to-auto-lthal-comparison.csv")
-truth_start_line = 86
-truth_end_line = 127
+truth = Path("/home/cowen/projects/erp-data/bert-standard-processed/rawavg_auto_features.csv")
+test = Path("/home/cowen/projects/erp-data/bert-lhip-test-2021-08-29/rawavg_features.csv")
+out_f = Path("/home/cowen/projects/erp-data/lhip-solo-process-vs-group-process-comparison.csv")
+# truth_start_line = 86
+# truth_end_line = 127
+# test_start_line = 2
+# test_end_line = 43
+
+truth_start_line = 2
+truth_end_line = 43
 test_start_line = 2
 test_end_line = 43
-
 
 def split_and_clean(csv_text: str, start_line: int, stop_line: int) -> List[Tuple[str]]:
     lines = csv_text.split("\n")
